026677ee817b7778dc401de1f8479fa19e6992e9
danizaghian <dani.zaghian@gmail.com>
2017-09-25 21:32:51 -0700

    initial commit

---
+++ app/controllers/forecasts_controller.rb
@@ -0,0 +1,18 @@
+class ForecastsController < ApplicationController
+  def index
+
+    if params[:city] && params[:state]
+      city = params[:city]
+      state = params[:state]
+    else
+      city = "sanfrancisco"
+      state = "ca"
+    end
+
+    @forecast = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22#{city}%2C%20#{state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
+    
+    @current_temp = @forecast['query']['results']['channel']['item']['condition']['temp']
+    @forecast_days = @forecast['query']['results']['channel']['item']['forecast']
+    @city_name = @forecast['query']['results']['channel']['location']['city']
+  end
+end
